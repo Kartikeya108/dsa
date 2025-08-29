a8e8301db6efd931ddb9f3a097a908ef5b077210
kartikeya108 <kartikeyaa94@gmail.com>
2025-08-29 12:10:12 +0530

    h

---
+++ A.java
@@ -0,0 +1,16 @@
+class A {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        
+        
+        for (int i = arr.length - 1; i > 0; i--) {
+            arr[i] = arr[i - 1];
+        }
+        
+        arr[0] =10;
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
